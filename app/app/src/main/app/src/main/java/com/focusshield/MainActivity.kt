0570b041be1e5d8d3b13424237402c311cf547a3
kgauwelotshidiso-sketch <kgauwelotshidiso@gmail.com>
2026-06-12 17:03:28 +0200

    Create MainActivity.kt

---
+++ app/app/src/main/app/src/main/java/com/focusshield/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.focusshield
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            FocusShieldApp()
+        }
+    }
+}
